bf808e45a2cba27d98a68850a1c07c08bdd3f312
Anshu Joshi <anshu.aj.joshi@gmail.com>
2022-12-18 20:59:26 +0530

    add my-image

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {useState} from 'react'
-import me from '../../assets/me.png'
+import heroMe from '../../assets/hero-me.png'
 import hello from '../../assets/hello.gif';
 import AnshuResume from '../../assets/AnshuResume.pdf'
+import './Mainpage.css'
 // import hero from '../../assets/hero.png'
 
 function MainPage() {
@@ -13,11 +14,10 @@ function MainPage() {
       <div className='hero-header'>
         <p className='hero-small-heading '>Hey <img src={hello} class="hello-img"/>,</p>
         <h1 className='hero-big-heading'>Anshu Joshi,</h1>
-        <h1 className='hero-big-heading secondry-big-heading'>I'm a </h1>
+        <h1 className='hero-big-heading secondry-big-heading'>I'm a <br/></h1>
         <p className='hero-desc'>
         I am a full stack developer and a UI/UX designer. I am currently doing my BTech in computer-science from IIIT Gwalior , Madhya Pradesh.I am currently in my 2nd year of Engineering.
         </p>
-      </div>
       <div className='download-form'>
       <button
         className='hero-btn primary-btn'
@@ -32,9 +32,10 @@ function MainPage() {
         </a>
       </button>
         </div>
-        {/* <div>
-          <img src={hero}/>
-        </div> */}
+      </div>
+        <div>
+          <img className="herome" src={heroMe}/>
+        </div>
     </section>
     );
   }
