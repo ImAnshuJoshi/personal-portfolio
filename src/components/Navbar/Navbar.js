f182109dfe0da536a714dac11064790429d0e2bf
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-05-12 17:29:45 +0530

    Add basic navabr styling and contact-me page styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,34 +2,53 @@ import React from "react";
 import { navLinks } from "../../data.js";
 import { FaBars } from "react-icons/fa";
 import { AiFillCloseCircle } from "react-icons/ai";
-import "./Navbar.css";
+// import "./Navbar.css";
 
 export default function Navbar({ mainRef }) {
-  const [navIsOpen, setNavIsOpen] = React.useState(false);
-  const [lastScrollTop, setLastScrollTop] = React.useState(0);
+  // const [navIsOpen, setNavIsOpen] = React.useState(false);
+  const [lastScrollTop, setLastScrollTop] = React.useState(1);
   const navRef = React.useRef(null);
   const childRef = React.useRef(null);
   const containerRef = React.useRef(null);
 
-  React.useEffect(() => {
-    let navHeight = childRef.current.getBoundingClientRect().height;
-    if (navIsOpen) {
-      // containerRef.current.style.height = `${navHeight + 5}px`;
-      // mainRef.current.style.filter = `blur(5px)`;
-    } else {
-      // containerRef.current.style.height = `0px`;
-      // mainRef.current.style.filter = `blur(0px)`;
-      // containerRef.current.style.height = `${navHeight + 0}px`;
-      // mainRef.current.style.filter = `blur(0px)`;
-    }
-    // eslint-disable-next-line
-  }, [navIsOpen]);
+  // React.useEffect(() => {
+  //   let navHeight = childRef.current.getBoundingClientRect().height;
+  //   if (navIsOpen) {
+  //     // containerRef.current.style.height = `${navHeight + 5}px`;
+  //     // mainRef.current.style.filter = `blur(5px)`;
+  //   } else {
+  //     // containerRef.current.style.height = `0px`;
+  //     // mainRef.current.style.filter = `blur(0px)`;
+  //     // containerRef.current.style.height = `${navHeight + 0}px`;
+  //     // mainRef.current.style.filter = `blur(0px)`;
+  //   }
+  //   // eslint-disable-next-line
+  // }, [navIsOpen]);
 
   React.useEffect(() => {
     window.addEventListener("scroll", changeNavOnScroll);
     return () => window.removeEventListener("scroll", changeNavOnScroll);
     // eslint-disable-next-line
   }, [lastScrollTop]);
+
+  // React.useLayoutEffect(() => {
+  //   function changeNavOnScroll() {
+  //     let scrollTop = window.scrollY || document.documentElement.scrollTop;
+  //     let height = navRef.current.getBoundingClientRect().height;
+
+  //     if (scrollTop === 0) {
+  //       navRef.current.style.top = `0`;
+  //     } else {
+  //       navRef.current.classList.add("nav-high-brightness");
+  //       navRef.current.style.top = `-${height}px`;
+  //     }
+
+  //     setLastScrollTop(scrollTop);
+  //   }
+
+  //   window.addEventListener("scroll", changeNavOnScroll);
+  //   return () => window.removeEventListener("scroll", changeNavOnScroll);
+  // }, []);
 
   // function toggleNav() {
   //   setNavIsOpen(!navIsOpen);
@@ -57,10 +76,7 @@ export default function Navbar({ mainRef }) {
           <ul className="nav-links flex" ref={childRef}>
             {navLinks.map((link, index) => {
               return (
-                <li
-                  key={index}
-                  className="links-list-item pd-side-medium"
-                >
+                <li key={index} className="links-list-item pd-side-medium">
                   <a href={link.url} className="fancy-link">
                     {link.name}
                   </a>
